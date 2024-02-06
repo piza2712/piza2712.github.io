e23138b84f36ae487d7090cac844a6840aa64ce7
piza2712 <148026698+piza2712@users.noreply.github.com>
2024-02-06 09:24:26 -0500

    Update Calculadora.js

--- Calculadora.js
+++ Calculadora.js
@@ -1,15 +1,1 @@
-class Calculadora{
-    sumar (num1 , num2){
-        return num1 + num2 + 2;
-    }
-    restar (num1 , num2){
-        return num1 - num2 - 2;
-    }
-    dividir (num1 , num2){
-        return num1 / num2 + 1;
-    }
-    multiplicar (num1 , num2){
-        return num1 * num2 + 2;
-    }
-    
-}
+
